bd54a57edfdcd92e2b2e9899c58498a502639175
oumaima regague <oumregague@gmail.com>
2025-10-27 12:22:34 +0100

    seconde

---
+++ ClientRestTemplate/src/main/java/org/sid/clientresttemplate/controller/EtudiantClientController.java
@@ -0,0 +1,4 @@
+package org.sid.clientresttemplate.controller;
+
+public class EtudiantClientController {
+}
---
+++ ClientRestTemplate/src/main/java/org/sid/clientresttemplate/service/EtudiantWebClient.java
@@ -0,0 +1,4 @@
+package org.sid.clientresttemplate.service;
+
+public class EtudiantWebClient {
+}
