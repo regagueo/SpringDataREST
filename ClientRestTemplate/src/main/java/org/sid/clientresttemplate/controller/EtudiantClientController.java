1f3d0d63f5fc05d37c2f1456de7e1c499487c109
oumaima regague <oumregague@gmail.com>
2025-10-27 12:28:13 +0100

    3

--- ClientRestTemplate/src/main/java/org/sid/clientresttemplate/controller/EtudiantClientController.java
+++ ClientRestTemplate/src/main/java/org/sid/clientresttemplate/controller/EtudiantClientController.java
@@ -1,4 +1,45 @@
 package org.sid.clientresttemplate.controller;
 
+import org.sid.clientresttemplate.service.EtudiantClientService;
+import org.sid.clientresttemplate.service.EtudiantWebClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/etudiants")
 public class EtudiantClientController {
-}
+    @Autowired
+    private EtudiantClientService etudiantClientService;
+    @Autowired
+    private EtudiantWebClient etudiantWebClient;
+    @GetMapping
+    public List<Map<String, Object>> getAllEtudiants() {
+        return etudiantClientService.getAllEtudiants();
+    }
+    @GetMapping("/{p}")
+    public Map<String, Object> getEtudiantById(@PathVariable("p") Integer id) {
+        return etudiantClientService.getEtudiantById(id);
+    }
+    @PostMapping
+    public void addEtudiant(@RequestBody Map<String, Object> etudiant) {
+        etudiantClientService.addEtudiant(etudiant);
+    }
+    @PutMapping("/{p}")
+    public String updateEtudiant(@PathVariable("p") Integer id,@RequestBody Map<String, Object> etudiant) {
+        return etudiantClientService.updateEtudiant(id,etudiant);
+    }
+    @DeleteMapping("/{p}")
+    public String deleteEtudiant(@PathVariable("p") Integer id) {
+        return etudiantClientService.deleteEtudiant(id);
+    }
+    @GetMapping("/nom/{n}")
+    public List<Map<String, Object>> getEtudiantsParNom(@PathVariable("n") String nom) {
+        return etudiantClientService.getEtudiantsByName(nom);
+    }
+    @GetMapping("/{p}/{s}/{t}") //http://localhost:8081/etudiants/0/2/nom,desc
+    public List<Map<String, Object>> getEtudiants(@PathVariable("p") int page,@PathVariable("s") int size, @PathVariable("t") String sort){
+        return etudiantClientService.getEtudiants(page, size, sort);
+    }}
--- ClientRestTemplate/src/main/java/org/sid/clientresttemplate/controller/WebClientController.java
+++ ClientRestTemplate/src/main/java/org/sid/clientresttemplate/controller/WebClientController.java
@@ -1,4 +1,25 @@
 package org.sid.clientresttemplate.controller;
 
+import org.sid.clientresttemplate.service.EtudiantWebClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class WebClientController {
+    @Autowired
+    EtudiantWebClient etudiantWebClient;
+    @GetMapping("/all")
+    public Map getEtudiants(){
+        return etudiantWebClient.getEtudiants();
+    }
+    @DeleteMapping("/supp/{p}") //http://localhost:8081/etudiants/supp/2
+    public String suppEtudiant(@PathVariable("p") long id){
+        return etudiantWebClient.deleteEtudiant(id);
+    }
+//................................
 }
